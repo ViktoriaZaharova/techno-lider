631f19b3119168b0815b733915b40cbeb15b26ac
Viktoria <zaharova181114@gmail.com>
2021-01-15 02:04:26 +0200

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -2,5 +2,20 @@ $('.main-slider').slick({
     slidesToShow: 1,
     arrows: true,
     dots: true,
-    fade: true
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
+
+$('.products-slider').slick({
+    slidesToShow: 4,
+});
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+
+    $('.products-slider').slick('setPosition');
+});
+
