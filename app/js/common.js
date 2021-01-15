7d8e83a5281f0cc97eceaf9a94f38d23d90aa0ed
Viktoria <zaharova181114@gmail.com>
2021-01-15 09:37:47 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -8,14 +8,6 @@ $('.main-slider').slick({
 });
 
 $('.products-slider').slick({
-    slidesToShow: 4,
-});
-
-$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-    $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-
-    $('.products-slider').slick('setPosition');
+    slidesToShow: 5,
 });
 
