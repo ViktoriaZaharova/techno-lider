a39702b0f3ad26205d105c345cdf8ca6979fd6be
Viktoria <zaharova181114@gmail.com>
2021-01-17 20:48:19 +0200

    media

--- app/js/common.js
+++ app/js/common.js
@@ -5,9 +5,50 @@ $('.main-slider').slick({
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 575,
+            settings: {
+                arrows: false,
+            }
+        }
+    ]
 });
 
 $('.products-slider').slick({
     slidesToShow: 5,
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 620,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
+$('.btn-burger').on("click", function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on("click", function () {
+    $('.mobile-menu').fadeOut();
+});
