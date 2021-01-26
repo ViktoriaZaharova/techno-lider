ad6ecfac70bed4a3d2498b57335b335d494c3a9d
Viktoria <zaharova181114@gmail.com>
2021-01-26 10:11:59 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -169,3 +169,18 @@ $('.list-estimate li').on('click', function () {
         $(stars[i]).addClass('selected');
     }
 });
+
+// accordeon
+function accordeon() {
+    var panel = $('.panel_heading');
+
+    if (panel.hasClass('in')) {
+        $('.in').find('.block_hover').slideDown();
+    }
+
+    $('.panel_heading .block_title').on('click', function () {
+        $(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+    });
+}
+
+accordeon();
